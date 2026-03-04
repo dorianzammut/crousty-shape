b6bd9279bd843e9c2c196d3dee34cb4c89d1ed71
dorianzm <hordeun@gmail.com>
2026-03-04 11:36:00 +0100

    corrige les problèmes CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:4201'],
+    origin: ['http://localhost:4200', 'http://localhost:4201', 'https://crousty-admin.onrender.com', 'https://crousty-shape.onrender.com'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
